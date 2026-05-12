4e0f9ff44d327018c1622d6852e43c03ea5aff5a
masumhossain-dev <masumhossain.dev.com>
2026-05-12 18:11:15 +0600

    show user data in UI from DB and delete button functionality execution

--- school_management_system/students/urls.py
+++ school_management_system/students/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile, name = 'student_profile')
+    path('profile/', views.profile, name = 'student_profile'),
+    path('deleteStd/<int:id>', views.deleteStudent, name = 'student_delete'),
 ]
--- school_management_system/students/views.py
+++ school_management_system/students/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from . import models
+from django.http import HttpResponse
 
 # Create your views here.
 def profile(response):
+    student_data = models.Students.objects.all()
     marks = [
         {"subject": "Bangla", "credit": 3, "mark": 73},
         {"subject": "English", "credit": 3, "mark": 67},
@@ -16,4 +19,9 @@ def profile(response):
         {"name": "Jobayer", "age": 24, "id": "011"},
         {"name": "Toufiq", "age": 25, "id": "016"},
     ]
-    return render(response, 'student/profile.html',context= {'marks': marks, 'stdInfo': stdInfo})
+    return render(response, 'student/profile.html',context= {'marks': marks, 'stdInfo': stdInfo, 'std_data': student_data})
+
+def deleteStudent(response, id):
+    student = models.Students.objects.get(id = id)
+    student.delete()
+    return HttpResponse('<h1>Student removed!</h1>')
